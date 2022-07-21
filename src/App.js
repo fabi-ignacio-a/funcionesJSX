88a716b9ed1e4d6fd288fb81a9c062b69bb8bebd
fabi_ignacio_ <100646521+fabi-ignacio-a@users.noreply.github.com>
2022-07-21 15:54:13 -0400

    Funciones de JSX en React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
 
+let ListNames = ({ children, names }) => {
+  return children(names);
+};
+
 export default function App() {
+  let names = [
+    { name: "Anna", id: 1 },
+    { name: "Jhon", id: 2 },
+    { name: "Julia", id: 3 }
+  ];
+  //Recuerda que si trabajas en una lista, debe tener una key
+  //Cuando se trabaja con React, esta dentro del mundo de JavaScript
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/*Esta es una forma de hacerlo*/}
+      {/*Forma A:
+        <ListNames names={names}>
+        {(names) => names.map((e) => <p key={e.id}> {e.name} </p>)}
+        </ListNames>*/}
+      {/*Esta es otra
+        Forma B:*/}
+      {names.map((e) => (
+        <p key={e.id}> {e.name} </p>
+      ))}
     </div>
   );
 }
